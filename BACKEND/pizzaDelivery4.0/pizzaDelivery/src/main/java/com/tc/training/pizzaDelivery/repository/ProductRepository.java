6beeb65312c46d224ce9fb0911860427a86ca03b
NAMRATHA141 <126678070+NAMRATHA141@users.noreply.github.com>
2023-08-23 15:07:55 +0530

    Delete BACKEND/pizzaDelivery4.0/pizzaDelivery directory

--- BACKEND/pizzaDelivery4.0/pizzaDelivery/src/main/java/com/tc/training/pizzaDelivery/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tc.training.pizzaDelivery.repository;
-
-import com.tc.training.pizzaDelivery.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
