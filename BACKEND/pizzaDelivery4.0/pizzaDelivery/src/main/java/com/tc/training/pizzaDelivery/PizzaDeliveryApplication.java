72c4413f08ea83cb9a8304f55e28574d4e211708
NAMRATHA141 <namratha.n@thoughtclan.com>
2023-09-25 15:14:21 +0530

    add files

--- BACKEND/pizzaDelivery4.0/pizzaDelivery/src/main/java/com/tc/training/pizzaDelivery/PizzaDeliveryApplication.java
+++ BACKEND/pizzaDelivery4.0/pizzaDelivery/src/main/java/com/tc/training/pizzaDelivery/PizzaDeliveryApplication.java
@@ -1,12 +1,18 @@
 package com.tc.training.pizzaDelivery;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PizzaDeliveryApplication {
-
-	public static void main(String[] args) {SpringApplication.run(PizzaDeliveryApplication.class, args);}
+	static Logger logger = LoggerFactory.getLogger(PizzaDeliveryApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaDeliveryApplication.class, args);
+		logger.info("App is starting");
+	}
 
 
 }
